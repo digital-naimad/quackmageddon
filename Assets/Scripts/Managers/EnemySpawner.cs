1c494c458f965e188ba8e20b02244a66d9e9f389
Damian Śremski <d.sremski@gmail.com>
2021-06-04 13:02:59 +0200

    add: EnemySpawner class

---
+++ Assets/Scripts/Managers/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private ObjectPooler pooler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+        pooler.SpawnFromPool("Duckie", transform.position, Quaternion.identity);
+    }
+
+}
