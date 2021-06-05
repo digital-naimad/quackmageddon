8b668af4eceb40238ed46d13f3fe098a246b2f02
Damian Śremski <d.sremski@gmail.com>
2021-06-05 06:20:25 +0200

    add: Explosion particle system prefab and Explosion controller script

---
+++ Assets/Scripts/ObjectControllers/ExplosionController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class ExplosionController : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem fireParticles = null;
+
+    [SerializeField]
+    private ParticleSystem debrisParticles = null;
+
+    [SerializeField]
+    private ParticleSystem sparksParticles = null;
+
+    public float animationLifespan = 3f;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="delay"></param>
+    public void LaunchAnimations(float delay = 0f)
+    {
+        gameObject.SetActive(true);
+
+        if (fireParticles != null)
+        {
+            fireParticles.Play();
+        }
+
+        if (debrisParticles != null)
+        {
+            debrisParticles.Play();
+        }
+
+        if (sparksParticles != null)
+        {
+            sparksParticles.Play();
+        }
+
+        StartCoroutine(HideWithDelay(animationLifespan));
+    }
+
+    IEnumerator HideWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        gameObject.SetActive(false);
+    }
+}
