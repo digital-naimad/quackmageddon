57da5b66b02c72af0fbb90d6fd98261f53119fd3
Damian Śremski <d.sremski@gmail.com>
2021-06-05 14:05:44 +0200

    add: MonoSingleton class
     implementing simplified version of the Singleton Pattern for MonoBehaviour

---
+++ Assets/Scripts/Utils/MonoSingleton.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+
+/// <summary>
+/// Implementation of Singleton Pattern
+/// </summary>
+/// <typeparam name="T">The type we want to make a singleton</typeparam>
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T self = null;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static T Instance
+    {
+        get
+        {
+            // Instance requiered for the first time, we look for it
+            if (self == null)
+            {
+                self = GameObject.FindObjectOfType(typeof(T)) as T;
+            }
+            return self;
+        }
+    }
+
+    private void Awake()
+    {
+        if (self == null)
+        {
+            self = this as T;
+        }
+        else if (self != this)
+        {
+            Destroy(this);
+            return;
+        }
+    }
+}
