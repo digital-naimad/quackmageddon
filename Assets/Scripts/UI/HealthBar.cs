f1e10eb3de73b1a3fb4f2df68d35654c261e4e74
Damian Śremski <d.sremski@gmail.com>
2021-06-06 05:33:03 +0200

    add: Health bar controller script

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 
+/// </summary>
+public class HealthBar : MonoBehaviour
+{
+    #region Inspector's fields
+    [SerializeField]
+    private Slider progressSlider;
+
+    [SerializeField]
+    private Image fillImage;
+
+    [SerializeField]
+    private Gradient progressGradient;
+
+    [SerializeField]
+    private short maxHealth = 100;
+
+    [SerializeField]
+    private short currentHealth = 0;
+    #endregion
+
+    public void SetMaxHealth(int value)
+    {
+        progressSlider.maxValue = value;
+        progressSlider.value = value;
+
+        fillImage.color = progressGradient.Evaluate(1f);
+    }
+
+    public void SetCurrentHealth(int value)
+    {
+        progressSlider.value = value;
+        fillImage.color = progressGradient.Evaluate(progressSlider.normalizedValue);
+    }
+
+}
