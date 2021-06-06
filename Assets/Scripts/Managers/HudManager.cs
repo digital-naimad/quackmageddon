2712cf1e11971ecab25fa34bd538b49cd432cb4e
Damian Śremski <d.sremski@gmail.com>
2021-06-06 12:31:21 +0200

    add: HUD manager
    Controlling changes of Score label and HP bar

---
+++ Assets/Scripts/Managers/HudManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Quackmageddon
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class HudManager : MonoBehaviour
+    {
+        #region Inspector fields
+        [SerializeField]
+        private HealthBar playerHealthBar;
+
+        [SerializeField]
+        private TMP_Text scoreLabel;
+
+        [SerializeField]
+        private Animator scoreLabelAnimator;
+
+        #endregion
+
+        #region Life cycle callbacks
+        private void Start()
+        {
+            playerHealthBar.SetMaxHealth(HealthManager.FullHealthValue);
+
+            GameplayEventsManager.Instance.RegisterListener(GameplayEventType.HealthUpdate, OnHealthUpdate);
+            GameplayEventsManager.Instance.RegisterListener(GameplayEventType.ScoreUpdate, OnScoreUpdate);
+        }
+
+        private void OnDestroy()
+        {
+            GameplayEventsManager.Instance.UnregisterListener(GameplayEventType.HealthUpdate, OnHealthUpdate);
+            GameplayEventsManager.Instance.UnregisterListener(GameplayEventType.ScoreUpdate, OnScoreUpdate);
+        }
+        #endregion
+
+        #region Event listeners
+        private void OnHealthUpdate(short currentHealth)
+        {
+            this.playerHealthBar.SetCurrentHealth(currentHealth);
+        }
+
+        private void OnScoreUpdate(short currentScore)
+        {
+            this.scoreLabel.text = currentScore.ToString().PadLeft(7, '0');
+
+            this.scoreLabelAnimator.Play("ScoreLabelBump", -1, 0f);
+        }
+        #endregion
+
+    }
+}
