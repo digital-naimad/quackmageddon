a1d425a7480fa20ef084171ac511ef871afb7eb5
Damian Śremski <d.sremski@gmail.com>
2021-06-07 10:29:36 +0200

    update: added method to deactivate all pooled objects listening for pause spawning event

--- Assets/Scripts/Managers/ObjectPooler.cs
+++ Assets/Scripts/Managers/ObjectPooler.cs
@@ -1,75 +1,102 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// 
-/// </summary>
-public class ObjectPooler : MonoBehaviour
+namespace Quackmageddon
 {
-    #region Inspector fields
-    [System.Serializable]
-    public struct Pool
+    /// <summary>
+    /// Universal object pooler. Implements Pool Pattern. Calls OnSpawn method of IPooledObject interface. Listens for Pause event to disable all pooled objects
+    /// </summary>
+    public class ObjectPooler : MonoBehaviour
     {
-        public string tag;
-        public GameObject prefab;
-        public short poolSize;
-    }
-
-    [SerializeField]
-    private List<Pool> poolsList;
-    #endregion
-
-    private Dictionary<string, Queue<GameObject>> poolDictionary;
-
-    private void Start()
-    {
-        poolDictionary = new Dictionary<string, Queue<GameObject>>();
-
-        foreach (Pool pool in poolsList)
+        #region Inspector fields
+        [System.Serializable]
+        public struct Pool
         {
-            Queue<GameObject> objectPool = new Queue<GameObject>();
+            public string tag;
+            public GameObject prefab;
+            public short poolSize;
+        }
 
-            for (int i = 0; i < pool.poolSize; i++)
+        [SerializeField]
+        private List<Pool> poolsList;
+        #endregion
+
+        private Dictionary<string, Queue<GameObject>> poolDictionary;
+
+        #region Life-cycle callbacks
+        private void Start()
+        {
+            GameplayEventsManager.Instance.RegisterListener(GameplayEventType.PauseSpawning, (foo) => { OnPause(); });
+
+            poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+            foreach (Pool pool in poolsList)
             {
-                GameObject newObject = Instantiate(pool.prefab);
-                newObject.SetActive(false);
-                objectPool.Enqueue(newObject);
+                Queue<GameObject> objectPool = new Queue<GameObject>();
+
+                for (int i = 0; i < pool.poolSize; i++)
+                {
+                    GameObject newObject = Instantiate(pool.prefab);
+                    newObject.SetActive(false);
+                    objectPool.Enqueue(newObject);
+                }
+
+                poolDictionary.Add(pool.tag, objectPool);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            GameplayEventsManager.Instance.UnregisterListener(GameplayEventType.PauseSpawning, (foo) => { OnPause(); });
+        }
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tag">string</param>
+        /// <param name="position"></param>
+        /// <param name="rotation"></param>
+        /// <returns></returns>
+        public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+        {
+            if (!poolDictionary.ContainsKey(tag))
+            {
+                Debug.Log("Warning: Pool with tag " + tag + " does not exist");
+                return null;
             }
 
-            poolDictionary.Add(pool.tag, objectPool);
-        }
-    }
+            GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+            objectToSpawn.SetActive(true);
+            objectToSpawn.transform.position = position;
+            objectToSpawn.transform.rotation = rotation;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="tag"></param>
-    /// <param name="position"></param>
-    /// <param name="rotation"></param>
-    /// <returns></returns>
-    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
-    {
-        if (!poolDictionary.ContainsKey(tag))
+            IPooledObject pooledObject = objectToSpawn.GetComponent<IPooledObject>();
+
+            if (pooledObject != null)
+            {
+                pooledObject.OnSpawn();
+            }
+
+            poolDictionary[tag].Enqueue(objectToSpawn);
+
+            return objectToSpawn;
+        }
+
+        /// <summary>
+        /// Deactivates all pooled objects by iterating dictionary and then queue of a GameObjects
+        /// </summary>
+        private void OnPause()
         {
-            Debug.Log("Warning: Pool with tag " + tag + " does not exist");
-            return null;
+            foreach (Pool pool in poolsList)
+            {
+                Queue<GameObject>.Enumerator enumerator = poolDictionary[pool.tag].GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    enumerator.Current.SetActive(false);
+                }
+            }
         }
 
-        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
-        objectToSpawn.SetActive(true);
-        objectToSpawn.transform.position = position;
-        objectToSpawn.transform.rotation = rotation;
-
-        IPooledObject pooledObject = objectToSpawn.GetComponent<IPooledObject>();
-
-        if (pooledObject != null)
-        {
-            pooledObject.OnSpawn();
-        }
-
-        poolDictionary[tag].Enqueue(objectToSpawn);
-
-        return objectToSpawn;
     }
-  
 }
