a4f83097e26970a9ccb034a0b8325e24d2cf0b30
Damian Śremski <d.sremski@gmail.com>
2021-06-10 04:03:07 +0200

    update: Enemy spawn type split into Static/Rushing/Boss

--- Assets/Scripts/Types/SpawnType.cs
+++ Assets/Scripts/Types/SpawnType.cs
@@ -1,10 +1,12 @@
 ﻿namespace Quackmageddon
 {
     /// <summary>
-    /// 
+    /// Defines all types of the spawned and pooled objects
     /// </summary>
     public enum SpawnType
     {
-        Enemy
+        StaticEnemy,
+        RushingEnemy,
+        Boss,
     }
 }
